60a3e93c7c2fafeca3859be7c845210b4c8ffdac
Pedro Martin <pataruco@users.noreply.github.com>
2021-04-07 19:32:00 +0100

    Create a GitHub action that print Hello on Dracula (#21)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hola');
+import * as core from '@actions/core';
+
+const start = async (): Promise<void> => {
+  try {
+    console.log(`Hello!`);
+    core.setOutput(
+      'compliance-evidence-url',
+      'https://i-am-an-url-to-a-compliance-folder.com',
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+start();
