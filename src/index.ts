543086095b9987381070d2cf7007ce77a3933306
Pedro Martin <pataruco@gmail.com>
2021-04-07 13:09:49 +0100

    Add githun actions core libraries

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hola');
+import core from '@actions/core';
+
+try {
+  console.log(`Hello!`);
+  core.setOutput(
+    'compliance-evidence-url',
+    'https://i-am-an-url-to-a-compliance-folder.com',
+  );
+} catch (error) {
+  core.setFailed(error.message);
+}
