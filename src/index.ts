df09062ee76723b8cb25953b03c658cf2ed4af5f
Pedro Martin <pataruco@gmail.com>
2021-04-07 15:23:21 +0100

    import actions/core into distribution

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import core from '@actions/core';
+import * as core from '@actions/core';
 
-try {
-  console.log(`Hello!`);
-  core.setOutput(
-    'compliance-evidence-url',
-    'https://i-am-an-url-to-a-compliance-folder.com',
-  );
-} catch (error) {
-  core.setFailed(error.message);
-}
+const start = async (): Promise<void> => {
+  try {
+    console.log(`Hello!`);
+    core.setOutput(
+      'compliance-evidence-url',
+      'https://i-am-an-url-to-a-compliance-folder.com',
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+start();
