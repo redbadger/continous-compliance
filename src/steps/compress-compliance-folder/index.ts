f9c9b81371f1df0c76e9fba6f4e0e096885b2d24
Pedro Martin <pataruco@gmail.com>
2021-04-08 15:11:25 +0100

    draft compress compliance folder step

---
+++ src/steps/compress-compliance-folder/index.ts
@@ -0,0 +1,21 @@
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import { COMPLIANCE_FOLDER } from '../../shared/constants';
+
+const createUnixTimeLabel = () =>
+  Number((new Date().getTime() / 1000).toFixed(0));
+
+const compressComplianceFolder = async (): Promise<void> => {
+  const unixTimeLabel = createUnixTimeLabel();
+
+  try {
+    await exec.exec(`zip -r ${unixTimeLabel}.zip ./${COMPLIANCE_FOLDER}`);
+    await exec.exec(`ls -lah`);
+  } catch (error) {
+    core.setFailed(
+      `Compress folder ${COMPLIANCE_FOLDER} failed with error: ${error.message}`,
+    );
+  }
+};
+
+export default compressComplianceFolder;
