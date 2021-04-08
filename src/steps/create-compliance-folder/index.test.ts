f8423b23a2310272a5295574e75d34e504aee1f9
Pedro Martin <pataruco@gmail.com>
2021-04-08 13:31:57 +0100

    Test createComplianceFolder 🧪

--- src/steps/create-compliance-folder/index.test.ts
+++ src/steps/create-compliance-folder/index.test.ts
@@ -1,5 +1,44 @@
+import * as io from '@actions/io';
+import * as core from '@actions/core';
+import createComplianceFolder from './index';
+import { COMPLIANCE_FOLDER } from '../../shared/constants';
+
 describe('createComplianceFolder', () => {
-  it('should create a compliance folder', () => {
-    expect(true).toBe(true);
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should create a compliance folder', async () => {
+    jest.spyOn(io, 'mkdirP').mockImplementation(jest.fn());
+
+    const mkdirPSpy = io.mkdirP as jest.Mock<any, any>;
+    // sanity check
+    expect(mkdirPSpy).toHaveBeenCalledTimes(0);
+
+    await createComplianceFolder();
+
+    expect(mkdirPSpy).toHaveBeenCalledTimes(1);
+    expect(mkdirPSpy).toHaveBeenCalledWith(COMPLIANCE_FOLDER);
+  });
+
+  it('should log an error when failed to create complaince folder', async () => {
+    jest.spyOn(io, 'mkdirP').mockRejectedValueOnce('💥');
+
+    const mkdirPSpy = io.mkdirP as jest.Mock<any, any>;
+    const errorSpy = core.error as jest.Mock<any, any>;
+
+    // sanity check
+    expect(mkdirPSpy).toHaveBeenCalledTimes(0);
+    expect(errorSpy).toHaveBeenCalledTimes(0);
+
+    try {
+      await createComplianceFolder();
+      expect(mkdirPSpy).toHaveBeenCalledTimes(1);
+    } catch (error) {
+      expect(errorSpy).toHaveBeenCalledTimes(1);
+      expect(errorSpy).toHaveBeenCalledWith(
+        `Error: failed to create compliance folder ${COMPLIANCE_FOLDER}, 💥.`,
+      );
+    }
   });
 });
