de2013790a727c93ff47d4eb6f6466a13c6281f2
Pedro Martin <pataruco@gmail.com>
2021-04-08 11:18:05 +0100

    Prevent copy test results into compliance folder when test-folder is not set

--- src/steps/copy-test-folder-into-compliance/index.ts
+++ src/steps/copy-test-folder-into-compliance/index.ts
@@ -1,22 +1,25 @@
 import * as core from '@actions/core';
-import * as exec from '@actions/exec';
 import * as io from '@actions/io';
 
 import { COMPLIANCE_FOLDER } from '../../shared/constants';
 
 const copyTestFolderIntoCompliance = async (): Promise<void> => {
   const testsFolderPath = core.getInput('tests-folder', { required: false });
-  try {
-    //  eslint-disable-next-line no-console
-    console.log({ testsFolderPath });
-    const options = { recursive: true, force: false };
 
-    await io.cp(testsFolderPath, COMPLIANCE_FOLDER, options);
+  const isTestFolderPathSet = Boolean(testsFolderPath);
 
-    await exec.exec('ls -lah');
-    await exec.exec(`ls -lah ./${COMPLIANCE_FOLDER}`);
-  } catch (error) {
-    core.setFailed(`Action failed with error ${error.message}`);
+  if (isTestFolderPathSet) {
+    try {
+      //  eslint-disable-next-line no-console
+      console.log({ testsFolderPath });
+
+      const options = { recursive: true, force: false };
+      await io.cp(testsFolderPath, COMPLIANCE_FOLDER, options);
+    } catch (error) {
+      core.setFailed(`Action failed with error ${error.message}`);
+    }
+  } else {
+    return;
   }
 };
 
