641e44b003a7453e12ac637fec30c309d1caa13a
Anubha Singh <45950125+anubha22@users.noreply.github.com>
2021-04-12 14:42:12 +0100

    Creating copy doc folder test script

---
+++ src/steps/copy-doc-folder-into-compliance/index.ts
@@ -0,0 +1,12 @@
+import * as exec from '@actions/exec';
+import * as core from '@actions/core';
+
+import { COMPLIANCE_FOLDER } from '../../shared/constants';
+
+const copyDocFolderIntoCompliance = async (): Promise<void> => {
+  try {
+    core.info('Test doc-folder');
+  } catch (error) {
+    throw new Error(`Error: failed something ${error.message}`);
+  }
+};
