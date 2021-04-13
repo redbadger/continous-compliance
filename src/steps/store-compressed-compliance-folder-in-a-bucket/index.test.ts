ed19e4f79550f4b4ecc135d7ae3b0536626d5f7c
Pedro Martin <pataruco@gmail.com>
2021-04-13 13:50:19 +0100

    Test error cases

--- src/steps/store-compressed-compliance-folder-in-a-bucket/index.test.ts
+++ src/steps/store-compressed-compliance-folder-in-a-bucket/index.test.ts
@@ -66,6 +66,7 @@ describe('storeCompressedComplianceFolderInABucket', () => {
     expect(getZipFilePathSpy).toHaveBeenCalledTimes(0);
     expect(StorageSpy).toHaveBeenCalledTimes(0);
 
+    // calling the function to test
     await storeCompressedComplianceFolderInABucket();
 
     expect(infoSpy).toHaveBeenCalledTimes(4);
@@ -108,5 +109,105 @@ describe('storeCompressedComplianceFolderInABucket', () => {
     expect(StorageSpy).toBeCalledWith({
       keyFilename: './service-account.json',
     });
+  });
+
+  it('should throw an error when createServiceAccountFile failed', async () => {
+    jest.spyOn(core, 'getInput').mockImplementation(jest.fn());
+    jest.spyOn(core, 'info').mockImplementation(jest.fn());
+    jest.spyOn(core, 'setOutput').mockImplementation(jest.fn());
+    jest.spyOn(promises, 'writeFile').mockImplementation(jest.fn());
+    jest.spyOn(glob, 'create').mockImplementation(jest.fn());
+    jest
+      .spyOn(storeIntoBucket, 'createServiceAccountFile')
+      .mockImplementation(() => {
+        throw new Error('💥');
+      });
+    jest
+      .spyOn(storeIntoBucket, 'getZipFilePath')
+      .mockImplementation(async () => '/home/runner/count-dracula/1234.zip');
+
+    const getInputSpy = core.getInput as jest.Mock<any, any>;
+    const infoSpy = core.info as jest.Mock<any, any>;
+    const setOutputSpy = core.setOutput as jest.Mock<any, any>;
+    const createServiceAccountFileSpy = storeIntoBucket.createServiceAccountFile as jest.Mock<
+      any,
+      any
+    >;
+    const getZipFilePathSpy = storeIntoBucket.getZipFilePath as jest.Mock<
+      any,
+      any
+    >;
+
+    const StorageSpy = googleCloudStorage.Storage;
+
+    // Sanity check
+    expect.assertions(7);
+    expect(getInputSpy).toHaveBeenCalledTimes(0);
+    expect(infoSpy).toHaveBeenCalledTimes(0);
+    expect(setOutputSpy).toHaveBeenCalledTimes(0);
+    expect(createServiceAccountFileSpy).toHaveBeenCalledTimes(0);
+    expect(getZipFilePathSpy).toHaveBeenCalledTimes(0);
+    expect(StorageSpy).toHaveBeenCalledTimes(0);
+
+    try {
+      // calling the function to test
+      await storeCompressedComplianceFolderInABucket();
+      expect(
+        async () => await storeCompressedComplianceFolderInABucket(),
+      ).toThrow('💥');
+    } catch (error) {
+      expect(error.message).toBe(
+        'Error: failed to send zip to Google Cloud storage, 💥',
+      );
+    }
+  });
+
+  it('should throw an error when getZipFilePath failed', async () => {
+    jest.spyOn(core, 'getInput').mockImplementation(jest.fn());
+    jest.spyOn(core, 'info').mockImplementation(jest.fn());
+    jest.spyOn(core, 'setOutput').mockImplementation(jest.fn());
+    jest.spyOn(promises, 'writeFile').mockImplementation(jest.fn());
+    jest.spyOn(glob, 'create').mockImplementation(jest.fn());
+    jest
+      .spyOn(storeIntoBucket, 'createServiceAccountFile')
+      .mockImplementation(jest.fn());
+    jest.spyOn(storeIntoBucket, 'getZipFilePath').mockImplementation(() => {
+      throw Error('💣');
+    });
+
+    const getInputSpy = core.getInput as jest.Mock<any, any>;
+    const infoSpy = core.info as jest.Mock<any, any>;
+    const setOutputSpy = core.setOutput as jest.Mock<any, any>;
+    const createServiceAccountFileSpy = storeIntoBucket.createServiceAccountFile as jest.Mock<
+      any,
+      any
+    >;
+    const getZipFilePathSpy = storeIntoBucket.getZipFilePath as jest.Mock<
+      any,
+      any
+    >;
+
+    const StorageSpy = googleCloudStorage.Storage;
+
+    // Sanity check
+    expect.assertions(7);
+    expect(getInputSpy).toHaveBeenCalledTimes(0);
+    expect(infoSpy).toHaveBeenCalledTimes(0);
+    expect(setOutputSpy).toHaveBeenCalledTimes(0);
+    expect(createServiceAccountFileSpy).toHaveBeenCalledTimes(0);
+    expect(getZipFilePathSpy).toHaveBeenCalledTimes(0);
+    expect(StorageSpy).toHaveBeenCalledTimes(0);
+
+    try {
+      // calling the function to test
+      await storeCompressedComplianceFolderInABucket();
+      expect(
+        async () => await storeCompressedComplianceFolderInABucket(),
+      ).toThrow('💣');
+    } catch (error) {
+      expect(error.message).toBe(
+        'Error: failed to send zip to Google Cloud storage, 💣',
+      );
+    }
   });
 });
