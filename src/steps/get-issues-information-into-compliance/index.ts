6290a497c61bb3469e3544ea2c9dd8b10b97385a
Pedro Martin <pataruco@gmail.com>
2021-04-13 15:36:20 +0100

    Testing github context

--- src/steps/get-issues-information-into-compliance/index.ts
+++ src/steps/get-issues-information-into-compliance/index.ts
@@ -4,7 +4,11 @@ import * as core from '@actions/core';
 const getIssuesInformationIntoCompliance = async (): Promise<void> => {
   const ghToken = core.getInput('github-token');
 
-  console.log({ ghToken });
+  const context = github.context;
+
+  const octokit = github.getOctokit(ghToken);
+
+  console.log({ context });
 };
 
 export default getIssuesInformationIntoCompliance;
