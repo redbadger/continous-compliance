744e065fd26fd0cbe229116bfbe2dcf5031d3a19
Pedro Martin <pataruco@gmail.com>
2021-04-14 15:57:13 +0100

    Set next steps

--- src/steps/get-issues-information-into-compliance/index.ts
+++ src/steps/get-issues-information-into-compliance/index.ts
@@ -1,37 +1,79 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
 
+import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods';
+
+type PullRequestSearchResponse = RestEndpointMethodTypes['search']['issuesAndPullRequests']['response'];
+type PullRequestListCommitsResponse = RestEndpointMethodTypes['pulls']['listCommits']['response'];
+type PullRequestSearchResponseItems = RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items'];
+
+// components["schemas"]["issue-search-result-item"]
+
+// TODO:
+// Type gitEvidence object
+// write file into compliance folder
+// Abstract into small function
+
+interface GitHubEvidence {
+  pull_request: PullRequestSearchResponse['data']['items'];
+  commits?: PullRequestListCommitsResponse['data'];
+}
+
 const getIssuesInformationIntoCompliance = async (): Promise<void> => {
+  let gitEvidence;
+
   const ghToken = core.getInput('github-token');
+  const isGhToken = Boolean(ghToken);
 
-  const {
-    context: {
-      repo: { repo, owner },
-      sha,
-    },
-  } = github;
+  if (isGhToken) {
+    const {
+      context: {
+        repo: { repo, owner },
+        sha,
+      },
+    } = github;
 
-  const octokit = github.getOctokit(ghToken);
+    const octokit = github.getOctokit(ghToken);
 
-  // Get PR by commit SHA
-  const q = `q=SHA=${sha}`;
-  const { data: prs } = await octokit.rest.search.issuesAndPullRequests({
-    q,
-  });
+    // Get PR by commit SHA
+    const q = `q=SHA=${sha}`;
 
-  const {
-    items: [pull_request],
-  } = prs;
-  const pull_number = pull_request.number;
+    const {
+      data: prs,
+    }: PullRequestSearchResponse = await octokit.rest.search.issuesAndPullRequests(
+      { q },
+    );
 
-  // GET commits by PR number
-  const { data: commits } = await octokit.rest.pulls.listCommits({
-    owner,
-    repo,
-    pull_number,
-  });
+    if (prs) {
+      const {
+        items: [pull_request],
+      } = prs;
 
-  console.log({ commits });
+      gitEvidence = {
+        pull_request,
+      };
+
+      const pull_number = pull_request.number;
+
+      const {
+        data: commits,
+      }: PullRequestListCommitsResponse = await octokit.rest.pulls.listCommits({
+        owner,
+        repo,
+        pull_number,
+      });
+
+      if (commits) {
+        gitEvidence = { ...gitEvidence, commits };
+      } else {
+        return;
+      }
+    } else {
+      return;
+    }
+  } else {
+    return;
+  }
 };
 
 export default getIssuesInformationIntoCompliance;
