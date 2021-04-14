a67195f222d6f0714515416c0217e3c6a9fee161
Pedro Martin <pataruco@gmail.com>
2021-04-14 09:56:25 +0100

    Draft GH API request to get PR by commit

--- src/steps/get-issues-information-into-compliance/index.ts
+++ src/steps/get-issues-information-into-compliance/index.ts
@@ -4,16 +4,24 @@ import * as core from '@actions/core';
 const getIssuesInformationIntoCompliance = async (): Promise<void> => {
   const ghToken = core.getInput('github-token');
 
-  const context = github.context;
+  const {
+    context: {
+      repo: { repo, owner },
+      sha,
+      eventName,
+    },
+  } = github;
 
   // const {} = context;
 
-  // const octokit = github.getOctokit(ghToken);
+  const octokit = github.getOctokit(ghToken);
+  const q = `q=SHA=${sha}`;
 
-  console.log({ ...context });
-  console.log({ github });
-  console.log('repo ==> ', context.repo);
-  console.log('issue ==> ', context.issue);
+  const { data } = await octokit.rest.search.issuesAndPullRequests({
+    q,
+  });
+
+  console.log({ data });
 };
 
 export default getIssuesInformationIntoCompliance;
@@ -143,4 +151,173 @@ Process env {
   ACTIONS_RUNTIME_TOKEN: '***',
   ACTIONS_CACHE_URL: 'https://artifactcache.actions.githubusercontent.com/WNL1JHryL2aicM5WkOBFBJnccB8bH1dOgQsr5Tn23lUiyCvXjr/'
 }
+*/
+
+/*
+
+{
+  payload: {
+    after: '8ab2e6ce5a541f2f06b9ffb77b6a8f3d72eac2a6',
+    base_ref: null,
+    before: '70343d6e9533a92f22d2a09938556c349240d008',
+    commits: [ [Object] ],
+    compare: 'https://github.com/redbadger/count-dracula/compare/70343d6e9533...8ab2e6ce5a54',
+    created: false,
+    deleted: false,
+    forced: false,
+    head_commit: {
+      author: [Object],
+      committer: [Object],
+      distinct: true,
+      id: '8ab2e6ce5a541f2f06b9ffb77b6a8f3d72eac2a6',
+      message: 'Trigger continous-compliance CI/CD',
+      timestamp: '2021-04-14T09:01:49+01:00',
+      tree_id: '6951569f7b2e3affc8af01ea0d85fade10ea4173',
+      url: 'https://github.com/redbadger/count-dracula/commit/8ab2e6ce5a541f2f06b9ffb77b6a8f3d72eac2a6'
+    },
+    organization: {
+      avatar_url: 'https://avatars.githubusercontent.com/u/265650?v=4',
+      description: null,
+      events_url: 'https://api.github.com/orgs/redbadger/events',
+      hooks_url: 'https://api.github.com/orgs/redbadger/hooks',
+      id: 265650,
+      issues_url: 'https://api.github.com/orgs/redbadger/issues',
+      login: 'redbadger',
+      members_url: 'https://api.github.com/orgs/redbadger/members{/member}',
+      node_id: 'MDEyOk9yZ2FuaXphdGlvbjI2NTY1MA==',
+      public_members_url: 'https://api.github.com/orgs/redbadger/public_members{/member}',
+      repos_url: 'https://api.github.com/orgs/redbadger/repos',
+      url: 'https://api.github.com/orgs/redbadger'
+    },
+    pusher: { email: 'pataruco@users.noreply.github.com', name: 'pataruco' },
+    ref: 'refs/heads/test-continous-compliance-v07',
+    repository: {
+      archive_url: 'https://api.github.com/repos/redbadger/count-dracula/{archive_format}{/ref}',
+      archived: false,
+      assignees_url: 'https://api.github.com/repos/redbadger/count-dracula/assignees{/user}',
+      blobs_url: 'https://api.github.com/repos/redbadger/count-dracula/git/blobs{/sha}',
+      branches_url: 'https://api.github.com/repos/redbadger/count-dracula/branches{/branch}',
+      clone_url: 'https://github.com/redbadger/count-dracula.git',
+      collaborators_url: 'https://api.github.com/repos/redbadger/count-dracula/collaborators{/collaborator}',
+      comments_url: 'https://api.github.com/repos/redbadger/count-dracula/comments{/number}',
+      commits_url: 'https://api.github.com/repos/redbadger/count-dracula/commits{/sha}',
+      compare_url: 'https://api.github.com/repos/redbadger/count-dracula/compare/{base}...{head}',
+      contents_url: 'https://api.github.com/repos/redbadger/count-dracula/contents/{+path}',
+      contributors_url: 'https://api.github.com/repos/redbadger/count-dracula/contributors',
+      created_at: 1617023946,
+      default_branch: 'main',
+      deployments_url: 'https://api.github.com/repos/redbadger/count-dracula/deployments',
+      description: null,
+      disabled: false,
+      downloads_url: 'https://api.github.com/repos/redbadger/count-dracula/downloads',
+      events_url: 'https://api.github.com/repos/redbadger/count-dracula/events',
+      fork: false,
+      forks: 0,
+      forks_count: 0,
+      forks_url: 'https://api.github.com/repos/redbadger/count-dracula/forks',
+      full_name: 'redbadger/count-dracula',
+      git_commits_url: 'https://api.github.com/repos/redbadger/count-dracula/git/commits{/sha}',
+      git_refs_url: 'https://api.github.com/repos/redbadger/count-dracula/git/refs{/sha}',
+      git_tags_url: 'https://api.github.com/repos/redbadger/count-dracula/git/tags{/sha}',
+      git_url: 'git://github.com/redbadger/count-dracula.git',
+      has_downloads: true,
+      has_issues: true,
+      has_pages: false,
+      has_projects: true,
+      has_wiki: true,
+      homepage: null,
+      hooks_url: 'https://api.github.com/repos/redbadger/count-dracula/hooks',
+      html_url: 'https://github.com/redbadger/count-dracula',
+      id: 352650506,
+      issue_comment_url: 'https://api.github.com/repos/redbadger/count-dracula/issues/comments{/number}',
+      issue_events_url: 'https://api.github.com/repos/redbadger/count-dracula/issues/events{/number}',
+      issues_url: 'https://api.github.com/repos/redbadger/count-dracula/issues{/number}',
+      keys_url: 'https://api.github.com/repos/redbadger/count-dracula/keys{/key_id}',
+      labels_url: 'https://api.github.com/repos/redbadger/count-dracula/labels{/name}',
+      language: 'TypeScript',
+      languages_url: 'https://api.github.com/repos/redbadger/count-dracula/languages',
+      license: [Object],
+      master_branch: 'main',
+      merges_url: 'https://api.github.com/repos/redbadger/count-dracula/merges',
+      milestones_url: 'https://api.github.com/repos/redbadger/count-dracula/milestones{/number}',
+      mirror_url: null,
+      name: 'count-dracula',
+      node_id: 'MDEwOlJlcG9zaXRvcnkzNTI2NTA1MDY=',
+      notifications_url: 'https://api.github.com/repos/redbadger/count-dracula/notifications{?since,all,participating}',
+      open_issues: 2,
+      open_issues_count: 2,
+      organization: 'redbadger',
+      owner: [Object],
+      private: false,
+      pulls_url: 'https://api.github.com/repos/redbadger/count-dracula/pulls{/number}',
+      pushed_at: 1618387315,
+      releases_url: 'https://api.github.com/repos/redbadger/count-dracula/releases{/id}',
+      size: 728,
+      ssh_url: 'git@github.com:redbadger/count-dracula.git',
+      stargazers: 1,
+      stargazers_count: 1,
+      stargazers_url: 'https://api.github.com/repos/redbadger/count-dracula/stargazers',
+      statuses_url: 'https://api.github.com/repos/redbadger/count-dracula/statuses/{sha}',
+      subscribers_url: 'https://api.github.com/repos/redbadger/count-dracula/subscribers',
+      subscription_url: 'https://api.github.com/repos/redbadger/count-dracula/subscription',
+      svn_url: 'https://github.com/redbadger/count-dracula',
+      tags_url: 'https://api.github.com/repos/redbadger/count-dracula/tags',
+      teams_url: 'https://api.github.com/repos/redbadger/count-dracula/teams',
+      trees_url: 'https://api.github.com/repos/redbadger/count-dracula/git/trees{/sha}',
+      updated_at: '2021-04-13T13:18:35Z',
+      url: 'https://github.com/redbadger/count-dracula',
+      watchers: 1,
+      watchers_count: 1
+    },
+    sender: {
+      avatar_url: 'https://avatars.githubusercontent.com/u/10330222?v=4',
+      events_url: 'https://api.github.com/users/pataruco/events{/privacy}',
+      followers_url: 'https://api.github.com/users/pataruco/followers',
+      following_url: 'https://api.github.com/users/pataruco/following{/other_user}',
+      gists_url: 'https://api.github.com/users/pataruco/gists{/gist_id}',
+      gravatar_id: '',
+      html_url: 'https://github.com/pataruco',
+      id: 10330222,
+      login: 'pataruco',
+      node_id: 'MDQ6VXNlcjEwMzMwMjIy',
+      organizations_url: 'https://api.github.com/users/pataruco/orgs',
+      received_events_url: 'https://api.github.com/users/pataruco/received_events',
+      repos_url: 'https://api.github.com/users/pataruco/repos',
+      site_admin: false,
+      starred_url: 'https://api.github.com/users/pataruco/starred{/owner}{/repo}',
+      subscriptions_url: 'https://api.github.com/users/pataruco/subscriptions',
+      type: 'User',
+      url: 'https://api.github.com/users/pataruco'
+    }
+  },
+  eventName: 'push',
+  sha: '8ab2e6ce5a541f2f06b9ffb77b6a8f3d72eac2a6',
+  ref: 'refs/heads/test-continous-compliance-v07',
+  workflow: 'Continous compliance 🔍',
+  action: 'redbadgercontinous-compliance',
+  actor: 'pataruco',
+  job: 'continous-compliance',
+  runNumber: 56,
+  runId: 747534234
+}
+{
+  github: {
+    context: Context {
+      payload: [Object],
+      eventName: 'push',
+      sha: '8ab2e6ce5a541f2f06b9ffb77b6a8f3d72eac2a6',
+      ref: 'refs/heads/test-continous-compliance-v07',
+      workflow: 'Continous compliance 🔍',
+      action: 'redbadgercontinous-compliance',
+      actor: 'pataruco',
+      job: 'continous-compliance',
+      runNumber: 56,
+      runId: 747534234
+    },
+    getOctokit: [Function: getOctokit]
+  }
+}
+repo ==>  { owner: 'redbadger', repo: 'count-dracula' }
+issue ==>  { owner: 'redbadger', repo: 'count-dracula', number: undefined }
+
 */
