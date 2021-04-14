e1441e92ac58e4c99024d4e43b2bfb1d35749c29
Pedro Martin <pataruco@gmail.com>
2021-04-14 20:46:45 +0100

    Refactor and type definitions

--- src/steps/get-issues-information-into-compliance/index.ts
+++ src/steps/get-issues-information-into-compliance/index.ts
@@ -1,13 +1,12 @@
 import * as github from '@actions/github';
 import * as core from '@actions/core';
+import { GitHub } from '@actions/github/lib/utils';
 
 import { RestEndpointMethodTypes } from '@octokit/plugin-rest-endpoint-methods';
 
 type PullRequestSearchResponse = RestEndpointMethodTypes['search']['issuesAndPullRequests']['response'];
 type PullRequestListCommitsResponse = RestEndpointMethodTypes['pulls']['listCommits']['response'];
-type PullRequestSearchResponseItems = RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items'];
-
-// components["schemas"]["issue-search-result-item"]
+type PullRequestSearchResponseItem = RestEndpointMethodTypes['search']['issuesAndPullRequests']['response']['data']['items'][number];
 
 // TODO:
 // Type gitEvidence object
@@ -15,29 +14,26 @@ type PullRequestSearchResponseItems = RestEndpointMethodTypes['search']['issuesA
 // Abstract into small function
 
 interface GitHubEvidence {
-  pull_request: PullRequestSearchResponse['data']['items'];
+  pull_request?: PullRequestSearchResponseItem;
   commits?: PullRequestListCommitsResponse['data'];
 }
 
-const getIssuesInformationIntoCompliance = async (): Promise<void> => {
-  let gitEvidence;
+interface GetPullRequestByCommitSHA {
+  octokit: InstanceType<typeof GitHub>;
+  sha: string;
+}
 
-  const ghToken = core.getInput('github-token');
-  const isGhToken = Boolean(ghToken);
+let gitEvidence: GitHubEvidence = {
+  pull_request: undefined,
+  commits: undefined,
+};
 
-  if (isGhToken) {
-    const {
-      context: {
-        repo: { repo, owner },
-        sha,
-      },
-    } = github;
-
-    const octokit = github.getOctokit(ghToken);
-
-    // Get PR by commit SHA
+const getPullRequestByCommitSHA = async ({
+  octokit,
+  sha,
+}: GetPullRequestByCommitSHA) => {
+  try {
     const q = `q=SHA=${sha}`;
-
     const {
       data: prs,
     }: PullRequestSearchResponse = await octokit.rest.search.issuesAndPullRequests(
@@ -48,32 +44,82 @@ const getIssuesInformationIntoCompliance = async (): Promise<void> => {
       const {
         items: [pull_request],
       } = prs;
+      return pull_request;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
 
-      gitEvidence = {
-        pull_request,
-      };
+interface GetCommitsByPr {
+  octokit: InstanceType<typeof GitHub>;
+  owner: string;
+  repo: string;
+  pull_number: number;
+}
 
-      const pull_number = pull_request.number;
+const getCommitsByPr = async ({
+  octokit,
+  owner,
+  repo,
+  pull_number,
+}: GetCommitsByPr) => {
+  try {
+    const {
+      data: commits,
+    }: PullRequestListCommitsResponse = await octokit.rest.pulls.listCommits({
+      owner,
+      repo,
+      pull_number,
+    });
+    if (commits) {
+      return commits;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
 
-      const {
-        data: commits,
-      }: PullRequestListCommitsResponse = await octokit.rest.pulls.listCommits({
+const getIssuesInformationIntoComplianceFolder = async (): Promise<void> => {
+  const ghToken = core.getInput('github-token');
+  const isGhToken = Boolean(ghToken);
+
+  if (isGhToken) {
+    const octokit = github.getOctokit(ghToken);
+    const {
+      context: {
+        repo: { repo, owner },
+        sha,
+      },
+    } = github;
+
+    // Get PR by commit SHA
+    const pull_request = await getPullRequestByCommitSHA({ octokit, sha });
+
+    if (pull_request) {
+      gitEvidence = { ...gitEvidence, pull_request };
+      const { number: pull_number } = pull_request;
+
+      // Get commits by PR number
+      const commits = await getCommitsByPr({
+        octokit,
         owner,
         repo,
         pull_number,
       });
 
       if (commits) {
+        // TODO: create object and write into disk
         gitEvidence = { ...gitEvidence, commits };
       } else {
-        return;
+        core.warning(`No commits associated with PR #${pull_number}`);
       }
     } else {
-      return;
+      core.warning(`Pull request associated with commit ${sha} not found`);
     }
   } else {
     return;
   }
 };
 
-export default getIssuesInformationIntoCompliance;
+export default getIssuesInformationIntoComplianceFolder;
