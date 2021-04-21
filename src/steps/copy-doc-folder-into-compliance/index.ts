8a4ac73580c1a86adb149b3009a1f1c7c5798ad4
Pedro Martin <pataruco@gmail.com>
2021-04-21 10:57:17 +0100

    Draft JSDoc style comments

--- src/steps/copy-doc-folder-into-compliance/index.ts
+++ src/steps/copy-doc-folder-into-compliance/index.ts
@@ -3,16 +3,30 @@ import * as io from '@actions/io';
 
 import { COMPLIANCE_FOLDER } from '../../shared/constants';
 
+/**
+ * @name copyDocFolderIntoCompliance
+ * @returns {void}
+ * @exports copyDocFolderIntoCompliance
+ * @function
+ * @async
+ * @description
+ * If `docs-folders` input is given it would copy the contents of the folder and paste it into compliance folder
+ * @exports If `docs-folders` input is given it would copy the contents of the folder and paste it into compliance folder
+ */
 const copyDocFolderIntoCompliance = async (): Promise<void> => {
+  // Get input 'docs-folder' path from the user
   const docsFolderPath = core.getInput('docs-folder', { required: false });
+  // Check if input exists
   const isDocsFolderPathSet = Boolean(docsFolderPath);
 
   if (isDocsFolderPathSet) {
+    // Logging into the console
     try {
       core.info(
         `Copy documents folder from ${docsFolderPath} into the folder ${COMPLIANCE_FOLDER}`,
       );
 
+      // Copying docs-folder into 'compliance-folder'
       const options = { recursive: true, force: false };
       await io.cp(docsFolderPath, COMPLIANCE_FOLDER, options);
     } catch (error) {
--- src/steps/create-compliance-folder/index.ts
+++ src/steps/create-compliance-folder/index.ts
@@ -3,12 +3,25 @@ import * as core from '@actions/core';
 
 import { COMPLIANCE_FOLDER } from '../../shared/constants';
 
+/**
+ * @name createComplianceFolder
+ * @function
+ * @async
+ * @description
+ * Create a a folder called compliance in the root of the project
+ * @module createComplianceFolder
+ * @returns {void}
+ * @description
+ * Create a compliance folder on root
+ */
+
 const createComplianceFolder = async (): Promise<void> => {
   try {
+    // Logging into the console
     core.info(`Creating Compliance folder on ${COMPLIANCE_FOLDER}.`);
-
+    // Create Compliance folder on root
     await io.mkdirP(COMPLIANCE_FOLDER);
-
+    // Logging into the console
     core.info(`Compliance folder created 📂.`);
   } catch (error) {
     throw new Error(
